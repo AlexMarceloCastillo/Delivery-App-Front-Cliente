cd289edd4199bf2a8a7bbf06129947882c0fde20
Valentín Leguizamón <valentin.leguizamon.sevilla@gmail.com>
2021-04-21 22:42:06 -0300

    Se agrega pagina provisoria de Menú

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CarritoService } from '../../services/carrito/carrito.service';
+
+import { ItemCarrito } from '../../models/itemCarrito.interface';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public foods:any[] = [
+    {
+      id:'001',
+      name:"food 01",
+      img: "https://place-hold.it/500x300",
+      price:100,
+    },
+    {
+      id:'002',
+      name:"food 02",
+      img: "https://place-hold.it/500x300",
+      price:250
+    },
+    {
+      id:'003',
+      name:"food 03",
+      img: "https://place-hold.it/500x300",
+      price:300
+    },
+    {
+      id:'004',
+      name:"food 04",
+      img: "https://place-hold.it/500x300",
+      price:145
+    },
+    {
+      id:'005',
+      name:"food 05",
+      img: "https://place-hold.it/500x300",
+      price:400
+    },
+    {
+      id:'006',
+      name:"food 06",
+      img: "https://place-hold.it/500x300",
+      price:350
+    }
+  ];
+
+  constructor(private cartSvc: CarritoService) { }
 
   ngOnInit(): void {
   }
 
+  public addItemCart(item: any, e: Event): void {
+    e.preventDefault();
+    let cartItem: ItemCarrito = {
+      id: item.id,
+      name: item.name,
+      img: item.img,
+      price: item.price,
+      cant: 1
+    }
+    this.cartSvc.addItem(cartItem);
+    e.stopPropagation();
+  }
 }
